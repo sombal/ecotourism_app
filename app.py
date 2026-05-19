5c81949ded77b452b5b6d4e7938df2d57765e034
기석 <sombaly@hanmail.net>
2026-05-19 18:32:09 +0900

    7월 프로그램 추가

--- app.py
+++ app.py
@@ -72,7 +72,14 @@ if st.session_state.page == "main" and menu == "🏠 프로그램 목록":
         3: {"name": "정기 생태관광(6월) 양구 3차", "period": "2026년 6월 16일 (화) (당일)", "desc": "자연의 신비, 대암산 용늪으로 떠나는 양구 생태관광 프로그램", "max": 12, "emoji": "🏞️", "deadline": date(2026, 6, 1), "price": "회원:30,000 / 비회원:50,000", "image": "https://raw.githubusercontent.com/sombal/ecotourism_app/main/images/3.jpg"},
         4: {"name": "정기 생태관광(6월) 양구 4차", "period": "2026년 6월 25일 (목) (당일)", "desc": "자연의 신비, 대암산 용늪으로 떠나는 양구 생태관광 프로그램", "max": 12, "emoji": "🌲", "deadline": date(2026, 6, 10), "price": "회원:30,000 / 비회원:50,000", "image": "https://raw.githubusercontent.com/sombal/ecotourism_app/main/images/4.jpg"},
         5: {"name": "정기 생태관광(6월) 고창", "period": "2026년 6월 18일 (목) ~ 6월 19일 (금)", "desc": "나는 개똥벌레~ 여름을 맞이하는 반딧불이 생태관광 프로그램", "max": 20, "emoji": "🌲", "deadline": date(2026, 6, 11), "price": "회원:50,000 / 비회원:70,000", "image": "https://raw.githubusercontent.com/sombal/ecotourism_app/main/images/4.jpg"},
-    }
+        6: {"name": "정기 생태관광(7월)", 
+            "period": "2026년 7월 15일 (수) ~ 16일 (목)", 
+            "desc": "여름 숲 속 힐링과 생태교육이 함께하는 1박2일 프로그램", 
+            "max": 18, 
+            "emoji": "🌳", 
+            "deadline": date(2026, 7, 8), 
+            "price": "회원:45,000 / 비회원:65,000", 
+            "image": "https://raw.githubusercontent.com/sombal/ecotourism_app/main/images/6.jpg"},    }
 
     cols = st.columns(2)
 
